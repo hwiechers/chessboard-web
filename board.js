98dae14a93cec7cd8bad4c39efb553973f8400a3
Henri Wiechers <hwiechers@gmail.com>
2012-04-16 06:47:27 +0200

    Remove an existing piece when taking.

--- board.js
+++ board.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('.board img').draggable({revert: true, revertDuration: 0});
     $('.board td').droppable({
         drop: function(event, ui) {
-            $(this).append(ui.draggable);
+            $(this).empty().append(ui.draggable);
         }
     });
 });
